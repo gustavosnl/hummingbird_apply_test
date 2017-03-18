dda43597f5c803099ad2c12b1cdf3e750823d44c
Gustavo Lima <gustavosnl@gmail.com>
2017-03-18 01:29:14 -0300

    re-pushing classes

--- app/src/main/java/com/glima/hummingbird/FlowController.java
+++ app/src/main/java/com/glima/hummingbird/FlowController.java
@@ -1,8 +1,17 @@
 package com.glima.hummingbird;
 
+import android.content.Context;
+
+import com.glima.hummingbird.view.detail.MovieDetailsActivity;
+import com.glima.hummingbird.view.model.MovieItemViewModel;
+
 /**
  * Created by gustavo on 18/03/17.
  */
 
 public class FlowController {
+
+    public static void openDetailsActivity(Context originContext, MovieItemViewModel movieItemViewModel) {
+        originContext.startActivity(MovieDetailsActivity.newIntent(originContext, movieItemViewModel));
+    }
 }
--- app/src/main/java/com/glima/hummingbird/view/detail/MovieDetailsActivity.java
+++ app/src/main/java/com/glima/hummingbird/view/detail/MovieDetailsActivity.java
@@ -1,8 +1,52 @@
 package com.glima.hummingbird.view.detail;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.glima.hummingbird.R;
+import com.glima.hummingbird.databinding.ActivityMovieDetailBinding;
+import com.glima.hummingbird.view.BaseActivity;
+import com.glima.hummingbird.view.model.MovieItemViewModel;
+
 /**
  * Created by gustavo on 17/03/17.
  */
 
-public class MovieDetailsActivity {
-}
+public class MovieDetailsActivity extends BaseActivity {
+        private static final String BUNDLE_KEY_SHOW = "movie";
+
+        private MovieItemViewModel viewModel;
+
+        public static Intent newIntent(Context context, MovieItemViewModel movie) {
+            Intent intent = new Intent(context, MovieDetailsActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putSerializable(BUNDLE_KEY_SHOW, movie);
+            intent.putExtras(bundle);
+
+            return intent;
+        }
+
+        @Override
+        public void onCreate(Bundle bundle) {
+            super.onCreate(bundle);
+            viewModel = (MovieItemViewModel) getIntent().getSerializableExtra(BUNDLE_KEY_SHOW);
+            ((ActivityMovieDetailBinding) viewDataBinding).setMovie(viewModel);
+        }
+
+        @Override
+        protected void init() {
+        }
+
+        @Override
+        protected void setupToolbar() {
+            Toolbar toolbar = ((ActivityMovieDetailBinding) viewDataBinding).collapsingToolbar.toolbar;
+            setSupportActionBar(toolbar);
+        }
+
+        @Override
+        public int getLayout() {
+            return R.layout.activity_movie_detail;
+        }
+    }
